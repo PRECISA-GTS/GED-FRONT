a88aa6a80d145a528fc63c2bfba16fcdf885058d
robertodelavi <roberto.delavy@gmail.com>
2024-04-25 15:13:31 -0300

    ...

--- src/configs/api.js
+++ src/configs/api.js
@@ -3,5 +3,5 @@ import axios from 'axios';
 export const api = axios.create({
     baseURL: process.env.NODE_ENV === 'development'
         ? process.env.NEXT_PUBLIC_BASE_URL
-        : 'https://app.gedagro.com.br/api2/'
+        : 'https://app.gedagro.com.br/api/'
 });
