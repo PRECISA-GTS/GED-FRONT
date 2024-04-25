92fa1fc3672a9f1b7439ab4dd9bd8acf2898f4d1
robertodelavi <roberto.delavy@gmail.com>
2024-04-25 14:24:17 -0300

    front apontando pra app api

--- src/configs/productionUrl.js
+++ src/configs/productionUrl.js
@@ -1,4 +1,4 @@
-const URL_PRODUCTION = 'https://app.gedagro.com.br/api2/';
+const URL_PRODUCTION = 'https://app.gedagro.com.br/api/';
 export default URL_PRODUCTION;
 
 //? Comando pro git ignorar o arquivo entre as branches locais (após add e commit):
