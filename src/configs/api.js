75a489ed64f3db15f005bd9f43724325c4fdc619
robertodelavi <roberto.delavy@gmail.com>
2024-06-20 11:31:37 -0300

    remoçao de NF no produto apontando quente

--- src/configs/api.js
+++ src/configs/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: process.env.NODE_ENV === 'development'
-        ? process.env.NEXT_PUBLIC_BASE_URL
-        : 'https://demo.gedagro.com.br/api/'
+  baseURL: process.env.NODE_ENV === 'development'
+    ? process.env.NEXT_PUBLIC_BASE_URL
+    : 'https://app.gedagro.com.br/api/'
 });
