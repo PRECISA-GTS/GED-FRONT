7b6df956256aec79f36e89ce2b3c78e3d097ec97
robertodelavi <roberto.delavy@gmail.com>
2024-07-26 10:22:53 -0300

    Atualização sexta feira apresentação reuniao

--- src/configs/api.js
+++ src/configs/api.js
@@ -1,14 +1,8 @@
 import axios from 'axios';
 
-const URL = 'https://demo.gedagro.com.br/api/'
+const URL = 'https://app.gedagro.com.br/api/'
 
 export const api = axios.create({
-<<<<<<< HEAD
-    baseURL: process.env.NODE_ENV === 'development'
-        ? process.env.NEXT_PUBLIC_BASE_URL
-        : 'https://app.gedagro.com.br/api/'
-});
-=======
   baseURL: process.env.NODE_ENV === 'development'
     ? process.env.NEXT_PUBLIC_BASE_URL
     : URL
@@ -17,4 +11,3 @@ export const api = axios.create({
 export const api_url = process.env.NODE_ENV === 'development'
   ? process.env.NEXT_PUBLIC_BASE_URL
   : URL
->>>>>>> 404ef1ac69ea77a7a58d23eafb664f4abf3777e1
