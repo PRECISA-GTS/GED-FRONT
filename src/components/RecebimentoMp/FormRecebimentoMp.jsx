2609746069a3774186750b4163a7fca6aaf6f8e8
robertodelavi <roberto.delavy@gmail.com>
2024-08-06 16:19:14 -0300

    Correções gerais

--- src/components/RecebimentoMp/FormRecebimentoMp.jsx
+++ src/components/RecebimentoMp/FormRecebimentoMp.jsx
@@ -383,6 +383,7 @@ const FormRecebimentoMp = ({ id, model }) => {
 
     const conclusionForm = async values => {
         // sendPdfToServer(id, blobSaveReport, 'recebimento-mp')
+        setOpenModal(false)
         values['conclusion'] = true
         await handleSubmit(onSubmit)(values)
     }
